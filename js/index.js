26bcf23b28966afc5a4334db73a17ae68771425f
yeoni2 <119648567+yeoni2@users.noreply.github.com>
2023-03-15 04:33:32 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,6 +8,12 @@ window.onload=function(){
         this["img"+i].style.backgroundSize="100% 100%";
         this["img"+i].addEventListener("click",this["big_func"+i] );
     }
+    // for(i=0; i<4; i++){
+    //     this["detail"+i]=document.getElementById("detail"+i);
+    //     this["img"+i].style.background="url(img/detail"+i+".jpg)";
+    //     this["detail"+i].style.backgroundSize="100% 100%";
+    //     this["detail"+i].addEventListener("click",this["detail_func"+i] );
+    // }    
 }
 function big_func0() {
     comm_func(0)
@@ -21,6 +27,9 @@ function big_func2() {
 function big_func3() {
     comm_func(3)
 }
+// function detail_func0(){
+//     design_func(0)
+// }
 function comm_func(js) {
     let big, pic, close;
     big=document.querySelector(".big");
@@ -34,3 +43,12 @@ function comm_func(js) {
         big.style.display="none";
     }
 }
+// function design_func(js){
+//     let closeup, end, deta;
+//     closeup=document.querySelector(".closeup");
+//     deta=document.querySelector("#deta");
+//     end=document.querySelector(".end");
+
+//     closeup.style.display="block";
+//     deta.src="img/detail"+js+".jpg";
+// }
