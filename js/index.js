71ba09deb5f004cebfb37aea6c5c47bf9b831fff
yeoni2 <119648567+yeoni2@users.noreply.github.com>
2023-03-15 22:39:49 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-let img0;
 window.onload=function(){
     AOS.init();
     $("html, body").animate({ "scrollTop": "0" },100);
@@ -8,12 +7,11 @@ window.onload=function(){
         this["img"+i].style.backgroundSize="100% 100%";
         this["img"+i].addEventListener("click",this["big_func"+i] );
     }
-    // for(i=0; i<4; i++){
-    //     this["detail"+i]=document.getElementById("detail"+i);
-    //     this["img"+i].style.background="url(img/detail"+i+".jpg)";
-    //     this["detail"+i].style.backgroundSize="100% 100%";
-    //     this["detail"+i].addEventListener("click",this["detail_func"+i] );
-    // }    
+    for(a=0; a<4; a++){
+        this["detail"+a]=document.getElementById("detail"+a);
+        this["img"+a].style.background="url(img/detail"+a+".jpg)";
+        this["detail"+a].addEventListener("click",this["detail_func"+a] );
+    }
 }
 function big_func0() {
     comm_func(0)
@@ -27,9 +25,6 @@ function big_func2() {
 function big_func3() {
     comm_func(3)
 }
-// function detail_func0(){
-//     design_func(0)
-// }
 function comm_func(js) {
     let big, pic, close;
     big=document.querySelector(".big");
@@ -43,12 +38,20 @@ function comm_func(js) {
         big.style.display="none";
     }
 }
-// function design_func(js){
-//     let closeup, end, deta;
-//     closeup=document.querySelector(".closeup");
-//     deta=document.querySelector("#deta");
-//     end=document.querySelector(".end");
+function detail_func0(){
+    design_func(0)
+}
+function design_func(js){
+    let closeup, end, deta;
+    closeup=document.querySelector(".closeup");
+    deta=document.querySelector("#deta");
+    end=document.querySelector(".end");
 
-//     closeup.style.display="block";
-//     deta.src="img/detail"+js+".jpg";
-// }
+    closeup.style.display="block";
+    deta.src="img/detail"+js+".jpg";
+
+    end.onclick=function(){
+        closeup.style.display="none";
+    }
+    $(".closeup").animate({ "scrollTop": "0" },0);
+}
